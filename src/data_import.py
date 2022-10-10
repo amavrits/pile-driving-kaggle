a323aa270a1675c8dfd83d560099a828b661e40a
amavrits <52402817+amavrits@users.noreply.github.com>
2022-10-10 14:52:27 +0200

    Convolution for running features

--- src/data_import.py
+++ src/data_import.py
@@ -19,31 +19,75 @@ def find_idx_depth(z_finder, z_where):
     return idx
 
 def set_geometry_dict(df):
-    return {pile: df[df['Location ID']==pile]['Diameter [m]'].values[0]
-                 for pile in list(pd.unique(df['Location ID']))}
+    return {pile: (
+        df[df['ID'] == pile]['diameter'].values[0],
+        df[df['ID'] == pile]['Bottom wall thickness [mm]'].values[0]
+    ) for pile in list(pd.unique(df['ID']))}
 
-def amend_cpt_data(df_pile, df_cpt, ammended_columns=['qnet', 'friction']):
+def amend_cpt_data(df_pile, df_cpt, ammended_columns=['qnet', 'friction', 'base_res_plug', 'base_res_wall', 'Ic'],
+                   window_length=None, conv_type='custom'):
     dict_geom = set_geometry_dict(df_pile)
     df_pile[ammended_columns] = 0
     for (pile, geom) in dict_geom.items():
         z = df_cpt[df_cpt['ID'] == pile]['z [m]'].values
         fs = df_cpt[df_cpt['ID'] == pile]['fs [MPa]'].values
         qnet = df_cpt[df_cpt['ID'] == pile]['qnet [MPa]'].values
-        friction = np.cumsum(fs[:-1] * np.pi * geom * np.diff(z))
-        friction = np.append(friction, friction[-1])
-        idx = find_idx_depth(z_finder=df_pile[df_pile['Location ID'] == pile]['z [m]'].values,
+        area_base_plug = np.pi * (geom[0] / 2) ** 2
+        area_base_wall = np.pi * (geom[0] / 2) ** 2 - np.pi * ((geom[0] - geom[1] / 1_000) / 2) ** 2
+        friction = np.cumsum(fs[:-1] * np.pi * geom[0] * np.diff(z))
+        friction = np.pad(friction, (0, 1), 'constant', constant_values=(0, friction[-1]))
+        base_resistance_plug = qnet * area_base_plug
+        base_resistance_wall = qnet * area_base_wall
+
+        if window_length is None:
+            window_length = int(geom[0] / np.diff(df_cpt['z [m]'].values)[0])
+        qnet = convolve_input(x=qnet, window_length=window_length, conv_type=conv_type)
+        base_resistance_plug = convolve_input(x=base_resistance_plug, window_length=window_length, conv_type=conv_type)
+        base_resistance_wall = convolve_input(x=base_resistance_wall, window_length=window_length, conv_type=conv_type)
+
+        idx = find_idx_depth(z_finder=df_pile[df_pile['ID'] == pile]['z'].values,
                              z_where=df_cpt[df_cpt['ID'] == pile]['z [m]'].values)
-        df_pile.loc[df_pile['Location ID'] == pile, ammended_columns] = np.c_[qnet[idx], friction[idx]]
+
+        df_pile.loc[df_pile['ID'] == pile, ammended_columns] = np.c_[qnet[idx],
+                                                                     friction[idx],
+                                                                     base_resistance_plug[idx],
+                                                                     base_resistance_wall[idx],
+                                                                     df_cpt['Ic [-]'].values[idx]]
+
     return df_pile
 
-def load_data(cpt_file, pile_file):
+def convolve_pad(x, w):
+    y = np.convolve(x, w, mode='valid')
+    y = np.pad(y, (0, x.size-y.size), 'constant', constant_values=(0, y[-1]))
+    return y
+
+def convolve_input(x, w=None, window_length=None, conv_type='custom'):
+    if conv_type == 'sum':
+        w = np.ones(window_length)
+        y = convolve_pad(x, w)
+    elif conv_type == 'average':
+        w = np.ones(window_length) / window_length
+        y = convolve_pad(x, w)
+    elif conv_type == 'max':
+        dummy = np.tile(x.reshape(-1, 1), reps=(1, window_length))
+        for i in range(window_length):
+            dummy[:, i] = np.roll(dummy[:, i], -i)
+        y = dummy.max(axis=1)
+    else:
+        if w is None:
+            raise Exception('No custom weight given for convolution')
+        y = convolve_pad(x, w)
+        pass
+    return y
+
+def load_data(cpt_file, pile_file, window_length=None, conv_type='custom'):
     df_cpt = pd.read_csv(cpt_file)
     df_pile = pd.read_csv(pile_file)
     df_cpt.dropna(how='any', inplace=True)
     df_cpt.reset_index(drop=True, inplace=True)
     df_pile.dropna(how='any', inplace=True)
     df_pile.reset_index(drop=True, inplace=True)
-    df_pile = amend_cpt_data(df_pile=df_pile, df_cpt=df_cpt)
+
     df_pile.drop(columns=['ID'], inplace=True)
     rename_dict = {'Blowcount [Blows/m]': 'blowcount',
                    'z [m]': 'z',
@@ -53,28 +97,18 @@ def load_data(cpt_file, pile_file):
                    'Normalised hammer energy [-]': 'hammer_energy',
                    'Location ID': 'ID'}
     df_pile.rename(rename_dict, inplace=True, axis='columns')
+
+    df_pile = amend_cpt_data(df_pile=df_pile, df_cpt=df_cpt, window_length=window_length, conv_type=conv_type)
+
     return df_pile
 
 
 if __name__ == '__main__':
 
-    df_cpt = pd.read_csv('..\\data\\full_cpt_training_data_withnormalised.csv')
-    df_pile = pd.read_csv('..\\data\\training_data.csv')
-    df_geom = pd.read_csv('..\\data\\interdistance_data.csv')
+    cpt_file = '..\\data\\full_cpt_training_data_withnormalised.csv'
+    pile_file = '..\\data\\training_data.csv'
 
-    df_cpt.dropna(how='any', inplace=True)
-    df_cpt.reset_index(drop=True, inplace=True)
-    df_pile.dropna(how='any', inplace=True)
-    df_pile.reset_index(drop=True, inplace=True)
-    df_geom.dropna(how='any', inplace=True)
-    df_geom.reset_index(drop=True, inplace=True)
-
-    # df_geom = add_location_distind(df_geom)
-
-    # sns.pairplot(data=df_pile[df_pile['Location ID']=='EK'], vars=['qc [MPa]', 'Normalised ENTRHU [-]', 'Blowcount [Blows/m]'])
-
-    df_pile = amend_cpt_data(df_pile, df_cpt, ammended_columns=['qnet', 'friction'])
-
+    df_pile = load_data(cpt_file=cpt_file, pile_file=pile_file, conv_type='sum')
 
 
 
--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from data_import import load_data, find_idx_depth
 
 
@@ -9,36 +10,65 @@ if __name__ == '__main__':
     cpt_file = '..\\data\\full_cpt_training_data_withnormalised.csv'
     pile_file = '..\\data\\training_data.csv'
 
-    df_pile = load_data(cpt_file=cpt_file, pile_file=pile_file)
+    df = load_data(cpt_file=cpt_file, pile_file=pile_file, window_length=None, conv_type='average')
 
 
-    df_pile['qnet_sum'] = 0
-    # for pile in list(pd.unique(df_pile['ID'])):
-    for pile in ['EK']:
-        dummy = df_pile[df_pile['ID'] == pile]
-        qnet = dummy['qnet'].values
-        z = dummy['z'].values
-        d = dummy['diameter'].values[0]
-        qnet_sum = np.zeros_like(qnet)
-        for i, zz in enumerate(z):
-            idx = np.where(z<= zz+d)[0]
-            qnet_sum[i] = np.sum(qnet[idx])
-        df_pile.loc[df_pile['ID'] == pile, 'qnet_sum'] = qnet_sum
+    df = df[df['ID'] == 'EK']
 
 
-    df_pile = df_pile[df_pile['qnet_sum']!=0]
+    # sns.pairplot(data=df, vars=['qnet', 'friction', 'Ic', 'base_res_plug', 'base_res_wall', 'entrhu', 'blowcount'])
 
 
-    sns.pairplot(data=df_pile, vars=['qnet', 'qnet_sum', 'friction', 'blowcount', 'entrhu'])
+    # from sklearn.linear_model import LinearRegression
+    # from sklearn.metrics import mean_squared_error
+    #
+    # X = df[['qnet', 'friction', 'entrhu']].values
+    # y = df['blowcount'].values.reshape(-1, 1)
+    # reg = LinearRegression().fit(X, y)
+    # y_pred = reg.predict(X)
+    # reg.score(X, y)
+    # A = mean_squared_error(y, y_pred, squared=False)
 
 
+    from EM_algorithm import EM
+    from sklearn.cluster import KMeans
+    from scipy.stats import norm
     from sklearn.linear_model import LinearRegression
     from sklearn.metrics import mean_squared_error
 
-    X = df_pile[['qnet_sum', 'friction']].values
-    y = df_pile['blowcount'].values.reshape(-1, 1)
-    reg = LinearRegression().fit(X, y)
-    y_pred = reg.predict(X)
-    reg.score(X, y)
-    A = mean_squared_error(y, y_pred, squared=False)
+    class EM_GMR(EM):
+
+        def log_like_fn(self, par):
+            x = self.X_train[:, :-1]
+            y = self.X_train[:, -1]
+            y_hat = x.dot(par[0])
+            log_like = norm.logpdf(y, loc=y_hat, scale=par[1])
+            return log_like
+
+
+    def init_fn(X_train, n_clusters, Z=None):
+        x = X_train[:, :-1].reshape(-1, 1)
+        y = X_train[:, -1]
+        if Z is None:
+            kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(np.c_[x, y])
+            Z = kmeans.labels_
+        betas = np.zeros((n_clusters, x.shape[1] + 1))
+        sigma = np.zeros(n_clusters)
+        for i_cluster in range(n_clusters):
+            idx = np.where(Z == i_cluster)[0]
+            reg = LinearRegression().fit(x[idx], y[idx])
+            y_hat = reg.predict(x[idx])
+            residuals = (y[idx] - y_hat) ** 2
+            sigma[i_cluster] = np.sqrt(np.dot(residuals.T, residuals) / (x.shape[0] - x.shape[1]))
+            betas[i_cluster] = np.array([reg.intercept_, reg.coef_[0]])
+        mix_par_init = (betas, sigma)
+        mix_weights_init = np.array([sum(Z == i_cluster) for i_cluster in range(n_clusters)]) / x.shape[0]
+        return mix_par_init, mix_weights_init
+
+    data = np.c_[np.ones(len(df)), df[['friction', 'blowcount']].values]
+    em = EM_GMR(init_fn=init_fn, model_type='linear')
+    # em.EM_set_initialization(data, n_clusters=3, init_method='random')
+    # em.train()
+    em.multi_random_init(data, n_clusters=3, n_random_inits=100)
+
 
